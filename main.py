e1a26cebb691c3132ed0681d0f11804ec77fbb58
Xuncheng Liu <liuxunchenglxc@163.com>
2026-06-20 17:42:43 +0800

    fix bugs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from encodings.base64_codec import base64_encode
 import subprocess
 import argparse
+import base64
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("--BUZZHEAVIER_ID", required=True, help="BUZZHEAVIER_ID")
@@ -7,7 +9,7 @@ parser.add_argument("--BUZZHEAVIER_ID", required=True, help="BUZZHEAVIER_ID")
 args = parser.parse_args()
 
 # setup
-subprocess.run("bash setup.sh", shell=True, text=True)
+# subprocess.run("bash setup.sh", shell=True, text=True)
 
 # download
 download_list = [
@@ -28,4 +30,5 @@ for item in download_list:
 
 # upload
 for item in download_list:
-    subprocess.run(f"bash upload.sh {item["filename"]} {item["note"]} {args.BUZZHEAVIER_ID}", shell=True, text=True)
+    url = f'https://w.buzzheavier.com/p348490rwt76/{item["filename"]}?note={base64.b64encode(item["note"].encode("utf-8")).decode("utf-8")}'
+    subprocess.run(f"bash upload.sh {item["filename"]} {url} {args.BUZZHEAVIER_ID}", shell=True, text=True)
