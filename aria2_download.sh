9df2b618b55e65029118e9c3e48f03338766de1b
Xuncheng Liu <liuxunchenglxc@163.com>
2026-06-21 22:42:10 +0800

    add echo

--- aria2_download.sh
+++ aria2_download.sh
@@ -1,6 +1,10 @@
 url=$1
 name=$2
 
+echo "start aria2c"
+echo "url="$1
+echo "name="$2
+
 aria2c --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0" -c -s 16 -x 16 -k 1M $url -o "$name"
 
 # aria2c --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0" -s 1 -x 1 -k 1M $url -o "$name"
