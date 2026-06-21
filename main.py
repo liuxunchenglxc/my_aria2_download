de112d84e8c45741e39fc4e961b325482e6c3963
Xuncheng Liu <liuxunchenglxc@163.com>
2026-06-21 21:14:19 +0800

    fix ...

--- main.py
+++ main.py
@@ -31,5 +31,5 @@ for item in download_list:
 # upload
 for item in download_list:
     url = f'https://w.buzzheavier.com/p348490rwt76/{item["filename"]}?note={base64.b64encode(item["note"].encode("utf-8")).decode("utf-8")}'
-    bzid = f'"Authorization: Bearer {args.BUZZHEAVIER_ID}"'
-    subprocess.run(f"bash upload.sh {'"' + item["filename"] + '"'} {url} {bzid}", shell=True, text=True)
+    bzid = f"Authorization: Bearer {args.BUZZHEAVIER_ID}"
+    subprocess.run(f"bash upload.sh {'"' + item["filename"] + '"'} {url} {'"' + bzid + '"'}", shell=True, text=True)
