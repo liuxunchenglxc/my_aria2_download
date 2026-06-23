79e84fe77e53424cb633758aff977df7dca77089
Xuncheng Liu <liuxunchenglxc@163.com>
2026-06-24 01:12:41 +0800

    四发

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import subprocess
 import argparse
 import base64
 from get_cook import download_cook
+import os
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("--BUZZHEAVIER_ID", required=True, help="BUZZHEAVIER_ID")
@@ -30,10 +31,34 @@ download_list = [
     #     "note": "《玄学大佬回京：一卦算哭满朝文武2》第1~81集",
     #     "type": "ytdlp"
     # },
+    # {
+    #     "url": "https://www.youtube.com/watch?v=KacjAmJmgc4",
+    #     "filename": "KacjAmJmgc4.mkv",
+    #     "note": "《从死囚营崛起》第1~3季",
+    #     "type": "ytdlp"
+    # },
     {
-        "url": "https://www.youtube.com/watch?v=KacjAmJmgc4",
-        "filename": "KacjAmJmgc4.mkv",
-        "note": "《从死囚营崛起》第1~3季",
+        "url": "https://www.youtube.com/watch?v=m1E2MFifqJI",
+        "filename": "m1E2MFifqJI.mkv",
+        "note": "《穿成恋爱脑村姑，被祸害的全家有救了》第1~80集",
+        "type": "ytdlp"
+    },
+    {
+        "url": "https://www.youtube.com/watch?v=4b3DCaeMJhE",
+        "filename": "4b3DCaeMJhE.mkv",
+        "note": "《末日天灾：我用空间囤了百亿物资》第1~100集",
+        "type": "ytdlp"
+    },
+    {
+        "url": "https://www.youtube.com/watch?v=jG1O_nQIDOU",
+        "filename": "jG1OnQIDOU.mkv",
+        "note": "《全宗起飞之大佬到宗门当弟子》第1~54集",
+        "type": "ytdlp"
+    },
+    {
+        "url": "https://www.youtube.com/watch?v=kYuWmQrcN5U",
+        "filename": "kYuWmQrcN5U.mkv",
+        "note": "《全宗起飞之大佬到宗门当弟子》第2季",
         "type": "ytdlp"
     },
     # {
@@ -44,16 +69,18 @@ download_list = [
     # },
 ]
 
+def upload(item):
+    url = f'https://w.buzzheavier.com/p348490rwt76/{item["filename"]}?note={base64.b64encode(item["note"].encode("utf-8")).decode("utf-8")}'
+    bzid = f"Authorization: Bearer {args.BUZZHEAVIER_ID}"
+    subprocess.run(["bash", "upload.sh", item["filename"], url, bzid], text=True)
+    # 删除上传过的文件
+    os.remove(item["filename"])
+
 for item in download_list:
     if item["type"] == "aria2":
         subprocess.run(["bash", "aria2_download.sh", item["url"], item["filename"]], text=True)
     elif item["type"] == "ytdlp":
         subprocess.run(["bash", "ytdlp_download.sh", item["url"], item["filename"]], text=True)
+    upload(item)
 
 ####################
-
-# upload
-for item in download_list:
-    url = f'https://w.buzzheavier.com/p348490rwt76/{item["filename"]}?note={base64.b64encode(item["note"].encode("utf-8")).decode("utf-8")}'
-    bzid = f"Authorization: Bearer {args.BUZZHEAVIER_ID}"
-    subprocess.run(["bash", "upload.sh", item["filename"], url, bzid], text=True)
